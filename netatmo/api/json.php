b769439de70ecf1a8e39a91e34f48112b8eaf1a2
mq-sol <info@mq-sol.jp>
2016-10-29 17:35:01 +0900

    余分なソース削除

--- netatmo/api/json.php
+++ netatmo/api/json.php
@@ -16,13 +16,6 @@ $inner = array();
 $outer = array();
 foreach ($data["devices"] as $device) {
     //場所・気温・湿度・C02
-     $message .= sprintf(
-        "%s : %s°C, %s%%, %sppm" . PHP_EOL
-        , $device["station_name"]
-        , $device["dashboard_data"]["Temperature"]
-        , $device["dashboard_data"]["Humidity"]
-        , $device["dashboard_data"]["CO2"]
-    );
 
     $name = $device["station_name"];
     $temperature = $device["dashboard_data"]["Temperature"];
@@ -36,12 +29,6 @@ foreach ($data["devices"] as $device) {
     //屋外モジュールなど
     foreach ($device["modules"] as $module) {
         //モジュール名・気温・湿度
-        $message .= sprintf(
-            "%s : %s°C, %s%% " . PHP_EOL
-            , $module["module_name"]
-            , $module["dashboard_data"]["Temperature"]
-            , $module["dashboard_data"]["Humidity"]
-        );
         $current = date('Y-m-d H:i:s', $device["dashboard_data"]["time_utc"]);
         $name = $module["module_name"];
         $temperature = $module["dashboard_data"]["Temperature"];
